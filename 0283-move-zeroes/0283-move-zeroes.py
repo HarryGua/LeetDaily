53581dfd78f3f03571ae1345adddd0935d0bd4e5
Oreo_Gua <95754934+HarryGua@users.noreply.github.com>
2026-03-30 01:11:36 +1100

    Time: 4 ms (60.55%), Space: 20.3 MB (33.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -8,12 +8,8 @@ class Solution:
         2. 左指针指向已排序的尾部，右指针指向待处理数组的头部
         3. 右指针不断向右移动，每次右指针指向非零数，则做右指针对应的数交换，同时左指针右移
         """
-        n = len(nums)
         left = right = 0
-        while right < n:
+        for right in range(left, len(nums)):
             if nums[right] != 0:
-                temp = nums[right]
-                nums[right] = nums[left]
-                nums[left] = temp
+                nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-            right += 1
